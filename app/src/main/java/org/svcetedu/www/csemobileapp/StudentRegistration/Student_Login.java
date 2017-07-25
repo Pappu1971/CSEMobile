bc2a80609b4d931a0e571d89864fe4519f5619af
Pappu Singha <pappusingha3@gmail.com>
2017-07-25 08:10:04 +0530

    first commit

---
+++ app/src/main/java/org/svcetedu/www/csemobileapp/StudentRegistration/Student_Login.java
@@ -0,0 +1,15 @@
+package org.svcetedu.www.csemobileapp.StudentRegistration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.svcetedu.www.csemobileapp.R;
+
+public class Student_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student__login);
+    }
+}
