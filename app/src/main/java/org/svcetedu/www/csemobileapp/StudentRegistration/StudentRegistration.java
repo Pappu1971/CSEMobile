6aa86701ba5ef19392ff16836a697e33e72e23af
Pappu Singha <pappusingha3@gmail.com>
2017-07-26 10:57:02 +0530

    Updated

--- app/src/main/java/org/svcetedu/www/csemobileapp/StudentRegistration/StudentRegistration.java
+++ app/src/main/java/org/svcetedu/www/csemobileapp/StudentRegistration/StudentRegistration.java
@@ -24,6 +24,7 @@ public class StudentRegistration extends AppCompatActivity {
     private EditText nameField;
     private EditText emailField;
     private EditText passField;
+    private EditText rollField;
     private Button signUp;
     private FirebaseAuth mAuth;
     private ProgressDialog mProgress;
@@ -43,6 +44,7 @@ public class StudentRegistration extends AppCompatActivity {
         nameField = (EditText) findViewById(R.id.nameField);
         emailField = (EditText) findViewById(R.id.emailField);
         passField = (EditText) findViewById(R.id.passField);
+        rollField=(EditText)findViewById(R.id.rollField);
         signUp = (Button) findViewById(R.id.signUp);
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,11 +61,12 @@ public class StudentRegistration extends AppCompatActivity {
 
         final String name = nameField.getText().toString().trim();
         final String email = emailField.getText().toString().trim();
+        final String roll=rollField.getText().toString().trim();
         String pass = passField.getText().toString().trim();
 
-        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass)) ;
+        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(roll)&&!TextUtils.isEmpty(pass)) ;
         {
-            mProgress.setMessage("Signing UP....");
+            mProgress.setMessage("Registering Your Id...");
             mProgress.show();
             mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
@@ -73,6 +76,7 @@ public class StudentRegistration extends AppCompatActivity {
                         String user_id = mAuth.getCurrentUser().getUid();
                         DatabaseReference current_user_db = mDatabase.child(user_id);
                         current_user_db.child("name").setValue(name);
+                        current_user_db.child("roll").setValue(roll);
                         current_user_db.child("email").setValue(email);
                         current_user_db.child("image").setValue("default");
                         mProgress.dismiss();
--- app/src/main/java/org/svcetedu/www/csemobileapp/StudentRegistration/Student_Login.java
+++ app/src/main/java/org/svcetedu/www/csemobileapp/StudentRegistration/Student_Login.java
@@ -1,15 +1,139 @@
 package org.svcetedu.www.csemobileapp.StudentRegistration;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.svcetedu.www.csemobileapp.R;
 
 public class Student_Login extends AppCompatActivity {
 
+    private static long back_pressed;
+    private EditText emailstudent;
+    private EditText passstudent;
+    private Button login;
+    private TextView registeraccount;
+    private ProgressDialog mProgress;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabaseUser;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_student__login);
+
+        mProgress = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("User");
+        mDatabaseUser.keepSynced(true);
+        emailstudent = (EditText) findViewById(R.id.studentloginemail);
+        passstudent = (EditText) findViewById(R.id.studentloginpass);
+
+        login = (Button) findViewById(R.id.login);
+        registeraccount = (TextView) findViewById(R.id.register);
+        registeraccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register = new Intent(Student_Login.this, StudentRegistration.class);
+                startActivity(register);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkLogin();
+            }
+        });
+
     }
-}
+
+    private void checkLogin() {
+
+        String email = emailstudent.getText().toString().trim();
+        String pass = passstudent.getText().toString().trim();
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass)) {
+            mProgress.setMessage("Checking Login...");
+            mProgress.show();
+            mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        mProgress.dismiss();
+                        checkUserExist();
+                    } else {
+
+                        mProgress.dismiss();
+                        Toast.makeText(Student_Login.this, "Error Login", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void checkUserExist() {
+        final String user_id = mAuth.getCurrentUser().getUid();
+        mDatabaseUser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.hasChild(user_id)) {
+                    Intent loginIntent = new Intent(Student_Login.this, StudentDash.class);
+                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(loginIntent);
+                } else {
+                    Toast.makeText(Student_Login.this, "You need to setup Your account", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (back_pressed + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finish();
+            ActivityCompat.finishAffinity(this);
+            System.exit(0);
+        } else {
+            Toast.makeText(getBaseContext(), "Press once again to exit", Toast.LENGTH_SHORT).show();
+            back_pressed = System.currentTimeMillis();
+        }
+
+
+    }
+    }
+
